835eaad31d124a44b400b1939373add4dc9affd8
Jmax <jmax407@gmail.com>
2021-05-17 10:53:19 -0400

    update

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const { ApolloServer } = require('apollo-server-express');
 const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 const { typeDefs, resolvers } = require('./schemas');
 
 const app = express();
